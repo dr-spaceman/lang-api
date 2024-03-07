9796c38b5e04ff50c5367cab4d76046eb6e11808
Matt Berti <mat.berti@gmail.com>
2024-03-07 21:43:22 +0700

    rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express'
+import rateLimit from 'express-rate-limit'
 import 'dotenv/config'
 
 const app = express()
 const port = 3000
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+})
+
+app.use(apiLimiter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
